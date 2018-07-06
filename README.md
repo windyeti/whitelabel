a0d6adfa79f67274e937b5e0a6648698d1168060
Yegor Tikhanin <yegor.tikhanin@gmail.com>
2018-07-06 14:29:54 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+Its landing White Label.
+To run you need Node js 4.* and
+git clone git......
+cd floder_project
+npm i
+gulp
+
+Enjoy)
---
+++ gulpfile.js
@@ -0,0 +1,93 @@
+var gulp = require('gulp');
+var pug = require('gulp-pug');
+var scss = require('gulp-sass');
+var watch = require('gulp-watch');
+var plumber = require('gulp-plumber');
+var browserSync = require('browser-sync');
+var imagemin = require('gulp-imagemin');
+var pngquant = require('imagemin-pngquant');
+
+var path = {
+    build : {
+        html : './build',
+        css : './build/css',
+        image : './build/images',
+        font : './build/fonts',
+        favicon : './build'
+    },
+    src : {
+        pug : './src/pugs/pages/*.pug',
+        scss : './src/style/main.scss',
+        image : './src/images/*.*',
+        font : './src/fonts/*.*',
+        favicon : './src/*.*'
+    },
+    watch : {
+        pug : './src/pugs/**/*.pug',
+        scss : './src/style/**/*.scss',
+        image : './src/images/*.*',
+        font : './src/fonts/*.*',
+        favicon : './src/*.*'
+    }
+};
+gulp.task('server', function() {
+    browserSync({
+        server : {
+            baseDir : './build'
+        }
+    })
+});
+gulp.task('build:favicon', function() {
+    gulp.src(path.src.favicon)
+        .pipe(gulp.dest(path.build.favicon))
+        .pipe(browserSync.reload({stream: true}))
+});
+gulp.task('build:font', function() {
+    gulp.src(path.src.font)
+        .pipe(gulp.dest(path.build.font))
+        .pipe(browserSync.reload({stream: true}))
+});
+gulp.task('build:pug', function() {
+    gulp.src([path.src.pug])
+        .pipe(plumber())
+        .pipe(pug({pretty: '\t'}))
+        .pipe(gulp.dest(path.build.html))
+        .pipe(browserSync.reload({stream: true}))
+});
+gulp.task('build:scss', function() {
+    gulp.src(path.src.scss)
+        .pipe(scss())
+        .pipe(gulp.dest(path.build.css))
+        .pipe(browserSync.reload({stream: true}))
+});
+gulp.task('build:image', function () {
+    gulp.src(path.src.image) //Выберем наши картинки
+        .pipe(imagemin({ //Сожмем их
+            progressive: true,
+            svgoPlugins: [{removeViewBox: false}],
+            use: [pngquant()],
+            interlaced: true
+        }))
+        .pipe(gulp.dest(path.build.image))
+        .pipe(browserSync.reload({stream: true}))
+});
+gulp.task('watch', function() {
+    watch(path.watch.pug, function() {
+        gulp.start('build:pug')
+    });
+    watch(path.watch.scss, function() {
+        gulp.start('build:scss')
+    });
+    watch(path.watch.scss, function() {
+        gulp.start('build:image')
+    });
+    watch(path.watch.favicon, function() {
+        gulp.start('build:favicon')
+    });
+    watch(path.watch.font, function() {
+        gulp.start('build:font')
+    });
+});
+
+gulp.task('build', ['build:pug', 'build:scss', 'build:image', 'build:favicon']);
+gulp.task('default', ['build', 'server', 'watch']);
